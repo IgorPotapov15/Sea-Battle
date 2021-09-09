56688e3869742f5cef66134c786103d36765d1fa
IgorPotapov15 <ingvarr.h@gmail.com>
2021-09-09 12:59:19 +0300

    made closing chunks that are near ships

--- assets/app.js
+++ assets/app.js
@@ -4,27 +4,28 @@ const buttonThree = document.querySelector('#b3');
 const buttonTwo = document.querySelector('#b2');
 const buttonOne = document.querySelector('#b1');
 
+let playerShips = {
+  fourShip: {
+    rest: 1,
+    size: 4
+  },
+  threeShip: {
+    rest: 2,
+    size: 3
+  },
+  twoShip: {
+    rest: 3,
+    size: 2
+  },
+  oneShip: {
+    rest: 4,
+    size: 1
+  }
+}
 
-let fourShip = {
-  rest: 1,
-  size: 4
-};
+let closedChunks = [];
 
-let threeShips = {
-  rest: 2,
-  size: 3
-};
-
-let twoShips = {
-  rest: 3,
-  size: 2
-};
-let oneShips = {
-  rest: 4,
-  size: 1
-};
-
-let currentShip = fourShip;
+let currentShip = playerShips.fourShip;
 
 field.addEventListener('click', (e) => {
   if (!e.target.hasAttribute('data-col')) return;
@@ -33,29 +34,84 @@ field.addEventListener('click', (e) => {
   let currentCol = e.target.dataset.col;
   let currentRow = e.target.dataset.row;
 
-  if (document.querySelector(`div[data-row="${+currentRow - (currentShip.size - 1)}"][data-col="${currentCol}"]`) === null)
-  return;
+  if (spaceChecking(currentShip.size, currentRow, currentCol)) return;
+  // if (closingChunks(currentShip.size, currentRow, currentCol)) return;
+  // closingChunks(currentShip.size, currentRow, currentCol)
+  // for (let i = 1; i < closedChunks.length; i++) {
+  //   console.log(closedChunks[i].dataset.row)
+  // }
 
-  e.target.classList.add('red')
+  e.target.classList.add('red');
+  e.target.classList.remove('white');
+
   for (let i = 1; i < currentShip.size; i++) { 
     currentRow = currentRow - 1;
     document.querySelector(`div[data-row="${currentRow}"][data-col="${currentCol}"]`).classList.add('red');
+    document.querySelector(`div[data-row="${currentRow}"][data-col="${currentCol}"]`).classList.remove('white');
+
   }
   currentShip.rest = currentShip.rest - 1;
+  closing();
 })
 
 buttonFour.addEventListener('click', () => {
-  currentShip = fourShip;
+  currentShip = playerShips.fourShip;
 })
 
 buttonThree.addEventListener('click', () => {
-  currentShip = threeShips;
+  currentShip = playerShips.threeShip;
 })
 
 buttonTwo.addEventListener('click', () => {
-  currentShip = twoShips;
+  currentShip = playerShips.twoShip;
 })
 
 buttonOne.addEventListener('click', () => {
-  currentShip = oneShips;
+  currentShip = playerShips.oneShip;
 })
+
+function spaceChecking(shipSize, row, col) {
+  if (document.querySelector(`div[data-row="${+row - (shipSize - 1)}"][data-col="${col}"]`) === null) 
+  return true;
+
+  // if (document.querySelector(`div[data-row="${+row - (shipSize - 1)}"][data-col="${col}"]`) !== null && document.querySelector(`div[data-row="${+row - (shipSize - 1)}"][data-col="${col}"]`).classList.contains('closed'))
+  // return true;
+  let tempRow = row;
+  for (let i = 1; i <= shipSize; i++) {
+    if(document.querySelector(`div[data-row="${tempRow}"][data-col="${col}"]`) !== null && document.querySelector(`div[data-row="${tempRow}"][data-col="${col}"]`).classList.contains('closed')) {
+      return true;
+    }
+    tempRow = tempRow - 1;
+  }
+}
+
+// function closingChunks(shipSize, row, col) {
+//   for (let i = 1; i < shipSize; i++) {
+//     if (
+//       document.querySelector(`div[data-row="${row}"][data-col="${+col - 1}"]`).classList.contains('red') ||
+//       document.querySelector(`div[data-row="${row}"][data-col="${+col + 1}"]`).classList.contains('red') ||
+//       document.querySelector(`div[data-row="${+row + 1}"][data-col="${col}"]`).classList.contains('red') ||
+//       document.querySelector(`div[data-row="${+row - 1}"][data-col="${col}"]`).classList.contains('red')
+//       ) {
+//         console.log('1')
+//       }
+      
+//   }
+// }
+
+function closing() {
+  let white = document.querySelectorAll('.white');
+  white.forEach(i => {
+    if (
+      (document.querySelector(`div[data-row="${+(i.dataset.row)}"][data-col="${+(i.dataset.col) - 1}"]`) !== null && document.querySelector(`div[data-row="${+(i.dataset.row)}"][data-col="${+(i.dataset.col) - 1}"]`).classList.contains('red')) ||
+      (document.querySelector(`div[data-row="${+(i.dataset.row)}"][data-col="${+(i.dataset.col) + 1}"]`) !== null && document.querySelector(`div[data-row="${+(i.dataset.row)}"][data-col="${+(i.dataset.col) + 1}"]`).classList.contains('red')) ||
+      (document.querySelector(`div[data-row="${+(i.dataset.row) - 1}"][data-col="${+(i.dataset.col)}"]`) !== null && document.querySelector(`div[data-row="${+(i.dataset.row) - 1}"][data-col="${+(i.dataset.col)}"]`).classList.contains('red')) ||
+      (document.querySelector(`div[data-row="${+(i.dataset.row) + 1}"][data-col="${+(i.dataset.col)}"]`) !== null && document.querySelector(`div[data-row="${+(i.dataset.row) + 1}"][data-col="${+(i.dataset.col)}"]`).classList.contains('red'))
+    ) {
+      i.classList.add('closed')
+    } 
+  })
+
+}
+
+closing()
