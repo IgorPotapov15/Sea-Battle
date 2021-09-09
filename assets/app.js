10cc274a5f849d0665005638a9ce8578a6f959c8
IgorPotapov15 <ingvarr.h@gmail.com>
2021-09-09 17:29:46 +0300

    started making random

--- assets/app.js
+++ assets/app.js
@@ -5,25 +5,14 @@ const buttonTwo = document.querySelector('#b2');
 const buttonOne = document.querySelector('#b1');
 const turnX = document.querySelector('#turn-x');
 const turnY = document.querySelector('#turn-y');
+const randomBut = document.querySelector('#random');
 
 const axis = {
   main: 'y',
   second: 'x'
 }
 
-turnX.addEventListener(('click'), () => {
-  axis.main = 'x';
-  axis.second = 'y';
-  console.log(axis)
-});
-
-turnY.addEventListener(('click'), () => {
-  axis.main = 'y';
-  axis.second = 'x';
-  console.log(axis)
-});
-
-let playerShips = {
+const playerShips = {
   fourShip: {
     rest: 1,
     size: 4
@@ -42,13 +31,115 @@ let playerShips = {
   }
 }
 
+let totalShipsRest = 10;
+
 let closedChunks = [];
 
 let currentShip = playerShips.fourShip;
 
-field.addEventListener('click', (e) => {
-  if (!e.target.hasAttribute('data-col')) return;
+field.addEventListener('click', placingShip);
+randomBut.addEventListener('click', placingRandom);
+
+buttonFour.addEventListener('click', () => {
+  currentShip = playerShips.fourShip;
+})
+buttonThree.addEventListener('click', () => {
+  currentShip = playerShips.threeShip;
+})
+buttonTwo.addEventListener('click', () => {
+  currentShip = playerShips.twoShip;
+})
+buttonOne.addEventListener('click', () => {
+  currentShip = playerShips.oneShip;
+})
+
+turnX.addEventListener(('click'), () => {
+  axis.main = 'x';
+  axis.second = 'y';
+});
+
+turnY.addEventListener(('click'), () => {
+  axis.main = 'y';
+  axis.second = 'x';
+});
+
+function nextShips(current) {
+  if (current.size === 4) {
+    currentShip = playerShips.threeShip;
+  }
+  if (current.size === 3) {
+    currentShip = playerShips.twoShip;
+  }
+  if (current.size === 2) {
+    currentShip = playerShips.oneShip;
+  }
+  if (current.size === 1) {
+    return false;
+  }
+  return true;
+}
+
+function placingRandom() {
+  // do {
+    for (let i = 1; i < 1000; i++) {
+    let randomAxis = Math.random();
+    if (randomAxis <= 0.5) {
+      axis.main = 'y';
+      axis.second = 'x';
+    } else {
+      axis.main = 'x';
+      axis.second = 'y';
+    }
+    if (currentShip.rest === 0) {
+      nextShips(currentShip)
+      if (nextShips(currentShip) === false) {
+        return;
+      } else {
+        placingRandom()
+      console.log('Закончились корабли')
+      }
+    }
+    let randomCol = +(Math.random().toString().substr(2, 1)) + 1;
+    let randomRow = +(Math.random().toString().substr(2, 1)) + 1;
+    if (!document.querySelector(`div[data-row="${randomRow}"][data-col="${randomCol}"]`).classList.contains('white')) {
+      placingRandom();
+      console.log('Попало по красному')
+    }
+    if (spaceChecking(currentShip.size, randomRow, randomCol)) {
+      if (totalShipsRest > 0) {
+        placingRandom();
+      }
+      console.log('Не прошло проверку')
+      return;
+    }
+
+    document.querySelector(`div[data-row="${randomRow}"][data-col="${randomCol}"]`).classList.add('red');
+    document.querySelector(`div[data-row="${randomRow}"][data-col="${randomCol}"]`).classList.remove('white');
+
+    for (let i = 1; i < currentShip.size; i++) {
+      if (axis.main === 'y') {
+        randomRow = randomRow - 1;
+      } else {
+        randomCol = randomCol - 1;
+      }
+      document.querySelector(`div[data-row="${randomRow}"][data-col="${randomCol}"]`).classList.add('red');
+      document.querySelector(`div[data-row="${randomRow}"][data-col="${randomCol}"]`).classList.remove('white');
+      
+    }
+    currentShip.rest = currentShip.rest - 1;
+    totalShipsRest = playerShips.fourShip.rest + playerShips.threeShip.rest + playerShips.twoShip.rest + playerShips.oneShip.rest;
+    closing();
+    console.log(totalShipsRest)
+    }
+    if (totalShipsRest > 0) {
+      placingRandom();
+    }
+  // } while (totalShipsRest === 0);
+}
+
+function placingShip(e) {
   if (currentShip.rest === 0) return;
+  if (!e.target.hasAttribute('data-col')) return;
   if (!e.target.classList.contains('white')) return;
   
   let currentCol = e.target.dataset.col;
@@ -70,24 +161,10 @@ field.addEventListener('click', (e) => {
 
   }
   currentShip.rest = currentShip.rest - 1;
+  totalShipsRest = playerShips.fourShip.rest + playerShips.threeShip.rest + playerShips.twoShip.rest + playerShips.oneShip.rest;
   closing();
-})
-
-buttonFour.addEventListener('click', () => {
-  currentShip = playerShips.fourShip;
-})
-
-buttonThree.addEventListener('click', () => {
-  currentShip = playerShips.threeShip;
-})
-
-buttonTwo.addEventListener('click', () => {
-  currentShip = playerShips.twoShip;
-})
-
-buttonOne.addEventListener('click', () => {
-  currentShip = playerShips.oneShip;
-})
+  console.log(totalShipsRest)
+}
 
 function spaceChecking(shipSize, row, col) {
   let axisVar = axis.main === 'y' ? (document.querySelector(`div[data-row="${+row - (shipSize - 1)}"][data-col="${col}"]`) === null) :
